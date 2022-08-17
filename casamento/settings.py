24b0e5b0560dd9faaca96e277ecfaca218a4bb19
Daniel Ferreira <daniel.lucas.ferreira@outlook.com.br>
2022-08-17 20:59:34 -0300

    Update settings.py

--- casamento/settings.py
+++ casamento/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Configure Django App for Heroku
